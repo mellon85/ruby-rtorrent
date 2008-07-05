6383d517755de3d1f2f33fe3396914b604a31139
Dario Meloni <mellon85@gmail.com>
2008-07-05 12:39:00 +0200

    SCGI::Wrapper.wrap accepts custom headers too
    Added a new parameter to pass addictional headers to the scgi message.

--- scgi.rb
+++ scgi.rb
@@ -5,9 +5,12 @@ module SCGI
 
 class Wrapper
 
-    def self.wrap( content, uri, method="POST" )
+    def self.wrap( content, uri, method="POST", headers=[] )
         null="\0"
         header = "CONTENT_LENGTH\0#{content.length}\0SCGI#{null}1\0REQUEST_METHOD\0#{method}\0REQUEST_URI\0#{uri}\0"
+        headers.each do |h,v|
+            header << "#{h}\0#{v}\0"
+        end
         return "#{header.length}:#{header},#{content}"
     end
 
